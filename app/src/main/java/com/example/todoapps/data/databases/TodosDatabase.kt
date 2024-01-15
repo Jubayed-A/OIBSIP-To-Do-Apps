47fc19ffab5ce003b2c29972cdb38d7f0395b008
Jubayed-A <learnigprograming@gmail.com>
2024-01-15 21:34:05 +0600

    todo databases  file added

---
+++ app/src/main/java/com/example/todoapps/data/databases/TodosDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.todoapps.data.databases
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todoapps.data.dao.TodosDao
+import com.example.todoapps.data.entity.Todo
+
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
+abstract class TodosDatabase : RoomDatabase() {
+    abstract val todosDao : TodosDao
+
+    companion object{
+        private var INSTANCE: TodosDatabase? = null
+
+        fun getInstance(context: Context) : TodosDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,TodosDatabase::class.java,"todo_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+
+
+}
