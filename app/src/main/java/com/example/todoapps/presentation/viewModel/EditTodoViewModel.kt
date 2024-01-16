925c897d1985fbb001a48a7e02b3f1b885af71a8
Jubayed-A <learnigprograming@gmail.com>
2024-01-16 10:26:28 +0600

    EditTodoViewModel file arrange

--- app/src/main/java/com/example/todoapps/presentation/viewModel/EditTodoViewModel.kt
+++ app/src/main/java/com/example/todoapps/presentation/viewModel/EditTodoViewModel.kt
@@ -7,27 +7,27 @@ import androidx.lifecycle.viewModelScope
 import com.example.todoapps.data.dao.TodosDao
 import kotlinx.coroutines.launch
 
-class EditTodoViewModel(todoId : Long, private val dao: TodosDao) : ViewModel() {
+class EditTodoViewModel(todoId: Long, private val dao: TodosDao) : ViewModel() {
 
     val todo = dao.get(todoId)
     private val _navigateToList = MutableLiveData<Boolean>()
     val navigateToList: LiveData<Boolean> get() = _navigateToList
 
-    fun updateTodoItem(){
+    fun updateTodoItem() {
         viewModelScope.launch {
             todo.value?.let { dao.update(it) }
             _navigateToList.value = true
         }
     }
 
-    fun deleteTodoItem(){
+    fun deleteTodoItem() {
         viewModelScope.launch {
             todo.value?.let { dao.delete(it) }
             _navigateToList.value = true
         }
     }
 
-    fun onNavigatedToList(){
+    fun onNavigatedToList() {
         _navigateToList.value = false
     }
 
